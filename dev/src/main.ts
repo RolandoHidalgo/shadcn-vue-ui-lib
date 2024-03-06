016d34bd0f06a55d657d646ced786775d0d48684
SkidRow <51795604+RolandoHidalgo@users.noreply.github.com>
2024-03-06 11:12:20 -0500

    lib base created

--- dev/src/main.ts
+++ dev/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 
 // Using import aliases in Vite
-import MyLibrary from 'my-library'
+import MyLibrary from 'shadcn-vue-ui'
 import App from './App.vue'
 
 const app = createApp(App)
--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      'my-library': path.resolve(__dirname, '../src'),
+      'shadcn-vue-ui': path.resolve(__dirname, '../src'),
     },
   },
 })
